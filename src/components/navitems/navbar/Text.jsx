43ccd6af760ee08663a3274baf4f9834ced1af1b
than <than@linkedtec.com>
2015-05-24 21:39:11 -0700

    added font size

--- src/components/navitems/navbar/Text.jsx
+++ src/components/navitems/navbar/Text.jsx
@@ -8,26 +8,37 @@ var MenuItem = require('react-bootstrap/lib/MenuItem');
 var NavItemMixin = require("../../navItemMixin");
 var dispatcher = require("../../../dispatcher");
 var EventType = require("../../../eventType");
-const panel =
-<Popover title='Text'>
-  <strong>Font family</strong>
-  <DropdownButton eventKey={3} >
-    <MenuItem eventKey='1'>16</MenuItem>
-  </DropdownButton>
-</Popover>
-;
-const content =
-<ButtonToolbar>
-  <OverlayTrigger trigger='click' placement='bottom' overlay={panel}>
-    <i className={"fa fa-font fa-2x"} > </i>
-  </OverlayTrigger>
-</ButtonToolbar>
-;
+
 module.exports = React.createClass({
   mixins: [NavItemMixin], // Set mixins
+  getInitialState:function(){
+    return {
+      currentFamily:"normal",
+      currentSize:40,
+      currentBackground:"black"
+    };
+  },
+  handleChange: function (event) {
+      this.setState({currentSize: event.target.value});
+  },
   render : function(){
+    var panel =
+    <Popover title='Text'>
+      <strong>Font family</strong>
+      <DropdownButton eventKey={1} title={this.state.currentFamily} >
+        <MenuItem eventKey='1'>{this.state.currentFamily}</MenuItem>
+      </DropdownButton>
+      <br/>
+      <strong>Font size</strong>
+      <input type="number" name="quantity" min="20" max="80"
+        onChange={this.handleChange}  value={this.state.currentSize}/>
+    </Popover>;
     return  (
-      content
+      <ButtonToolbar>
+        <OverlayTrigger trigger='click' placement='bottom' overlay={panel}>
+          <i className={"fa fa-font fa-2x"} > </i>
+        </OverlayTrigger>
+      </ButtonToolbar>
     );
   }
 });
